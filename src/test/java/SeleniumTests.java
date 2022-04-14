f528612178bb24a9dda36adef04b40cf939dd6f3
Artur-Sch <runks@ya.ru>
2022-04-14 23:49:21 +0300

    fix task 6

--- src/test/java/SeleniumTests.java
+++ src/test/java/SeleniumTests.java
@@ -59,15 +59,27 @@ public class SeleniumTests {
     public void test6() {
         getAutorized();
         List<WebElement> menus = getElementsBy(By.xpath("//li[@id='app-']"));
+        String h1Text;
         for (int i = 1; i <= menus.size(); i++) {
             waitForVisibilityElement(getElementBy(By.cssSelector(String.format("ul#box-apps-menu > li#app-:nth-child(%d)", i)))).click();
+
             WebElement selectedElement = getElementBy(By.cssSelector("#box-apps-menu #app-.selected"));
             List<WebElement> menuElements = selectedElement.findElements(By.tagName("li"));
-            for (int j = 2; j <= menuElements.size(); j++) {
-                selectedElement = getElementBy(By.cssSelector("#box-apps-menu #app-.selected"));
-                selectedElement.findElement(By.cssSelector(String.format("li:nth-child(%d)", j))).click();
-
+            if (menuElements.size() > 0) {
+                for (int j = 2; j <= menuElements.size(); j++) {
+                    selectedElement = getElementBy(By.cssSelector("#box-apps-menu #app-.selected"));
+                    h1Text = waitForVisibilityElement(getElementBy(By.cssSelector("h1"))).getText();
+                    Assertions.assertNotNull(h1Text);
+//                    Assertions.assertEquals(h1Text, getElementBy(By.cssSelector(".docs .selected .name")).getText());
+                    selectedElement = selectedElement.findElement(By.cssSelector(String.format("li:nth-child(%d)", j)));
+                    selectedElement.click();
+                }
+            } else {
+                h1Text = waitForVisibilityElement(getElementBy(By.cssSelector("h1"))).getText();
+//                Assertions.assertEquals(h1Text, selectedElement.getText());
+                Assertions.assertNotNull(h1Text);
             }
+
         }
     }
 
@@ -91,6 +103,9 @@ public class SeleniumTests {
 
     public WebElement waitForVisibilityElement(WebElement element) {
         return wait.until(ExpectedConditions.visibilityOf(element));
+    }
+    public WebElement waitForClickableElement(WebElement element) {
+        return wait.until(ExpectedConditions.elementToBeClickable(element));
     }
 
     public List<WebElement> getElementsBy(By by) {
